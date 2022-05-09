0324829f17894d08f1a31522f3bd4d4c7e2de48e
Shreyas Paliwal <shreyaspaliwal21@gmail.com>
2022-05-10 00:43:35 +0530

    Search Flights Usecase

--- src/main/java/com/shreyas/flightreservation/repos/FlightRepository.java
+++ src/main/java/com/shreyas/flightreservation/repos/FlightRepository.java
@@ -1,9 +1,18 @@
 package com.shreyas.flightreservation.repos;
 
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.shreyas.flightreservation.entities.Flight;
 
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+
+	@Query("form Flight where departureCity=:departureCity and arrivalCity=:arrivalCity and dateOfDeparture=:dateOfDeparture")
+	List<Flight> findFlights(@Param(":departureCity") String from, 
+			@Param(":arrivalCity") String to, @Param(":dateOfDeparture") Date departureDate);
 	
 }
--- src/main/webapp/WEB-INF/jsps/findFlights.jsp
+++ src/main/webapp/WEB-INF/jsps/findFlights.jsp
@@ -8,8 +8,11 @@
 </head>
 <body>
 <h2>Find Flights:</h2>
-<form>
-
+<form action="findFlights" method="post">
+From: <input type="text" name="from" />
+To: <input type="text" name="to" />
+Departure Date: <input type="text" name="departureDate" />
+<input type="submit" value="search" />
 </form>
 </body>
 </html>
