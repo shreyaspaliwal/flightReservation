e424c629da2a1d822772dfd95a20a648559df5cf
Shreyas Paliwal <shreyaspaliwal21@gmail.com>
2022-05-15 21:26:20 +0530

    Containerize the application into Docker

--- src/main/java/com/shreyas/flightreservation/FlightreservationApplication.java
+++ src/main/java/com/shreyas/flightreservation/FlightreservationApplication.java
@@ -2,12 +2,19 @@ package com.shreyas.flightreservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 @Component
-public class FlightreservationApplication {
+public class FlightreservationApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FlightreservationApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FlightreservationApplication.class, args);
 	}
